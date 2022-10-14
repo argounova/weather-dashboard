83f3c3bebc5416549e513f2b339d7e624a0e6a69
Volantis <putzstuck@gmail.com>
2022-10-13 21:02:49 -0400

    added function to set lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ var lon;
 var country;
 var userZipInput;
 var requestGEO;
-var apiRootURL = 'http://api.openweathermap.org/geo/1.0/zip?';
+var apiRootGeoURL = 'http://api.openweathermap.org/geo/1.0/zip?';
 var requestGEO;
-// var requestURL = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={78bc832eeac2a213024b0c9cb066c83c}";
-var x;
+var apiRootURL = 'http://api.openweathermap.org/data/2.5/forecast?';
+var requestURL;
 
 $('input').keyup(function(e){
     e.preventDefault();
@@ -18,11 +18,11 @@ $('input').keyup(function(e){
 });
 
 $('#button-addon2').click(function setZip(){
-    var paramsString ='zip=34668,US&appid=78bc832eeac2a213024b0c9cb066c83c';
+    var paramsString = 'zip={zip},US&appid=78bc832eeac2a213024b0c9cb066c83c';
     var searchParams = new URLSearchParams(paramsString);
     searchParams.set('zip', userZipInput);
-    x = searchParams.toString();
-    requestGEO = apiRootURL+x;
+    var x = searchParams.toString();
+    requestGEO = apiRootGeoURL+x;
     console.log(requestGEO);
     getGeo(requestGEO);
 });
@@ -44,41 +44,16 @@ function getGeo(requestGEO){
         console.log(lat);
         console.log(lon);
         console.log(country);
+        setLatLon();
         });
 }
 
-
-
-
-// function getWeather(requestURL){
-//     fetch(requestURL)
-//         .then(function(response){
-//         return response.json();
-//         })
-//         .then(function(data){
-//         console.log(data);
-        // for(var i = 0; i < data.lenth; i++){
-
-            // var location = document.createElement('h1');
-            // var temp = document.createElement('p');
-            // var wind = document.createElement('p');
-            // var humidity = document.createElement('p');
-            // location.textContent = data[i].//insert location tag here//
-            // temp.textContent = data[i].//insert temp tag here//
-            // wind.textContent = data[i].//insert wind tag here//
-            // humidity.textContent = data[i].//insert humidity tag here//
-            // currentContainer.append(location);
-            // currentContainer.append(temp);
-            // currentContainer.append(wind);
-            // currentContainer.append(humidity);
-//         });       
-// };
-// $('input').keyup(function(e){
-//     e.preventDefault();
-//     console.log(e.target.value);
-//     userZipInput = $('input#locInput').val();
-//     console.log(userZipInput);
-// });
-
-// getGeo(requestGEO);
-// getWeather(requestURL);
+function setLatLon(){
+    var paramsString = 'lat={lat}&lon={lon}&appid=78bc832eeac2a213024b0c9cb066c83c';
+    var searchParams = new URLSearchParams(paramsString);
+    searchParams.set('lat', lat);
+    searchParams.set('lon', lon);
+    var x = searchParams.toString();
+    requestURL = apiRootURL+x;
+    console.log(requestURL);
+}
