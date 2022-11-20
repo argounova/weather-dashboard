fa54a97de0d4a8a7c64ba58cd930f2662ba6ebac
Volantis <putzstuck@gmail.com>
2022-11-19 21:01:00 -0500

    refactored function to set parameters for query string

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ var lon;
 var country;
 var userZipInput;
 var requestGEO;
-var apiRootGeoURL = 'http://api.openweathermap.org/geo/1.0/zip?';
-var requestGEO;
-var apiRootURL = 'http://api.openweathermap.org/data/2.5/weather?';
 var requestURL;
+const appid = '78bc832eeac2a213024b0c9cb066c83c';
+const apiRootGeoURL = 'http://api.openweathermap.org/geo/1.0/zip?';
+const apiRootURL = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiRootForecast = 'http://api.openweathermap.org/data/2.5/forecast/daily?'
+const openWeaApi = 'https://api.openweathermap.org/data/3.0/onecall?';
 // const dt = DateTime.local();
 
 $('input').keyup(function(e){
@@ -45,22 +47,24 @@ function getGeo(requestGEO){
         // console.log(lat);
         // console.log(lon);
         // console.log(country);
-        setLatLon();
+        setParams();
         });
 }
 
-function setLatLon(){
-    var paramsString = 'lat={lat}&lon={lon}&appid=78bc832eeac2a213024b0c9cb066c83c';
-    var searchParams = new URLSearchParams(paramsString);
+function setParams(){
+    let paramsString = 'lat={lat}&lon={lon}&exclude={part}&appid={APIkey}';
+    let searchParams = new URLSearchParams(paramsString);
     searchParams.set('lat', lat);
     searchParams.set('lon', lon);
-    var x = searchParams.toString();
-    requestURL = apiRootURL+x;
-    // console.log(requestURL);
-    getWeather();
+    searchParams.set('exclude', `minutely,hourly,alerts`);
+    searchParams.set('appid', appid);
+    requestURL = openWeaApi+searchParams.toString();
+    console.log(requestURL);
+    // getCurrent();
+    // getForecast();
 }
 
-function getWeather(){
+function getCurrent(){
     fetch(requestURL)
         .then(function(response){
         return response.json();
@@ -115,4 +119,14 @@ function handleCurrent(data) {
     newElement = document.createElement('p');
     newElement.append('Wind: '+windSpd+' mph');
     document.getElementById('currentCity').append(newElement);
+}
+
+function getForecast() {
+    fetch(requestFC)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
 }
